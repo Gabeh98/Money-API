26edd5472d3bd90e77d5a735ab34a24a526fefcc
Gabeh98 <bielhenry09@gmail.com>
2021-06-13 16:33:25 -0300

    Removendo migration com flyway do projeto e salvando sql list

---
+++ src/main/java/com/money/model/Pessoa.java
@@ -0,0 +1,52 @@
+package com.money.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="pessoa")
+public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long codigo;
+    @NotNull
+    @Size(min = 3,max = 20)
+    private String nome;
+    @NotNull
+    private boolean ativo;
+    @Embedded
+    private Endereco endereco;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
